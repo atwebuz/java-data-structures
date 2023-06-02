3a4fe805f14ac2d88717d8b55fd12f8189dd5332
Abdulaziz <abdusattorov2004@mail.ru>
2023-06-02 07:47:47 +0500

    foreach array

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,37 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] colors = new String[5];
+        colors[0] = "purple";
+        colors[1] = "blue";
+
+        System.out.println(Arrays.toString(colors));
+
+        colors[2] = "yello";
+
+        System.out.println(colors[0]);
+
+        for (int i=0 ; i<colors.length; i++){
+            System.out.println(colors[i]);
+        }
+
+        System.out.println("-- reverse for --");
+
+        for (int i = colors.length - 1; i>=0;i-- ){
+            System.out.println(colors[i]);
+        }
+
+        System.out.println("-- forEach --");
+        for(String color: colors){
+            System.out.println(color);
+        }
+
+        System.out.println("-- second way forEach Stream --");
+        Arrays.stream(colors).forEach(System.out::println);
+
+
     }
 }
