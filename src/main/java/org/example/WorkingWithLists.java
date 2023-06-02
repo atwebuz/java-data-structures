da31f5c6d66ae358bc16bbbb4955c14defd50fc6
Abdulaziz <abdusattorov2004@mail.ru>
2023-06-02 09:58:04 +0500

    ArrayList

---
+++ src/main/java/org/example/WorkingWithLists.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WorkingWithLists {
+    public static void main(String[] args) {
+
+        List<String> colors = new ArrayList<>();
+        colors.add("blue");
+        colors.add("purple");
+        colors.add("Yellow");
+        System.out.println(colors.size());
+        System.out.println(colors.contains("Yellow"));
+        System.out.println(colors);
+        // **********
+//        for(String color: colors){
+//            System.out.println(color);
+//        }
+        // **********
+//        colors.forEach(System.out::println);
+        // **********
+        for(int i = 0; i < colors.size(); i++){
+            System.out.println(colors.get(i));
+        }
+    }
+}
